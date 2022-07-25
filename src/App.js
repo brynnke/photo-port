1bec2c4001ccad4e794b15410aa381d98ae5b47d
brynnke <100329799+brynnke@users.noreply.github.com>
2022-07-25 13:45:40 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -10,31 +10,36 @@ function App() {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: 'portraits', description: 'Portraits of people in my life'},
-    { name: 'food', description: 'Delicious delicacies'},
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature'},
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
 
   const [contactSelected, setContactSelected] = useState(false);
-    return (
-        <div>
-          <Nav
-            categories={categories}
-            setCurrentCategory={setCurrentCategory}
-            currentCategory={currentCategory}
-            contactSelected={contactSelected}
-            setContactSelected={setContactSelected}
-          ></Nav>
-            <main>
-                <ContactForm></ContactForm>
-                <Gallery currentCategory={currentCategory}></Gallery>
-                <About></About>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
