676a4c450b7fee395cd29aa089efe7a1a9c227b1
brynnke <100329799+brynnke@users.noreply.github.com>
2022-07-25 13:05:23 -0500

    all changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Nav from './components/Nav';
 import About from "./components/About";
 import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
@@ -16,15 +17,20 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+
+  const [contactSelected, setContactSelected] = useState(false);
     return (
         <div>
           <Nav
             categories={categories}
             setCurrentCategory={setCurrentCategory}
             currentCategory={currentCategory}
+            contactSelected={contactSelected}
+            setContactSelected={setContactSelected}
           ></Nav>
             <main>
-                <Gallery></Gallery>
+                <ContactForm></ContactForm>
+                <Gallery currentCategory={currentCategory}></Gallery>
                 <About></About>
             </main>
         </div>
